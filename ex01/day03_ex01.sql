7d639d4660f4d955b6971e9bccf8dee923ede2be
Monkster <miki.rulll2012@gmail.com>
2026-05-06 23:23:56 +0300

    123

--- ex01/day03_ex01.sql
+++ ex01/day03_ex01.sql
@@ -1,6 +1,6 @@
 SELECT m.id
     FROM menu AS m
-LEFT JOIN person_order AS po
-    ON po.menu_id = m.id
-WHERE po.menu_id IS NULL
+EXCEPT
+    SELECT po.menu_id
+        FROM person_order AS po
 ORDER BY 1;
