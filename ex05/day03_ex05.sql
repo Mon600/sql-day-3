b7acb57eed5c4e5b8258e3e73f6a1b6d5e75d148
Monkster <miki.rulll2012@gmail.com>
2026-05-07 11:04:00 +0300

    fix: ex05

--- ex05/day03_ex05.sql
+++ ex05/day03_ex05.sql
@@ -1,12 +1,13 @@
-(SELECT pz.name
+(SELECT pz.name AS pizzeria_name
 FROM person_visits AS pv
     JOIN pizzeria AS pz ON pz.id = pv.pizzeria_id
     JOIN person AS p ON p.id = pv.person_id
 WHERE p.name = 'Andrey')
 EXCEPT
-(SELECT pz.name
+(SELECT pz.name AS pizzeria_name
 FROM person_order AS po
     JOIN person AS p ON p.id = po.person_id
     JOIN menu AS m ON m.id = po.menu_id
     JOIN pizzeria AS pz ON pz.id = m.pizzeria_id
-WHERE p.name = 'Andrey');
+WHERE p.name = 'Andrey')
+ORDER BY pizzeria_name;
