da00001420392858527f41fb589afd828be89a2d
Monkster <miki.rulll2012@gmail.com>
2026-05-08 21:49:42 +0300

    add: ex11

---
+++ ex11/day03_ex11.sql
@@ -0,0 +1,11 @@
+UPDATE menu
+    SET price = (price + (price * 0.1))::INTEGER
+WHERE pizza_name = 'greek pizza';
+
+SELECT *
+FROM menu
+WHERE pizza_name LIKE '%greek pizza%';
+
+-- UPDATE menu
+--     SET price = 800
+-- WHERE pizza_name = 'greek pizza';
